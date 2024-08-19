8cc97fe903a644e5f171e042a1b761487d61cc1d
Zhang Kun Jie <89339273+warriorthegreat@users.noreply.github.com>
2024-08-19 21:17:59 +0800

    Add files via upload

---
+++ 張焜傑的網站/chatgpt網站範例/張焜傑個人網站範例.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var menuContent = document.getElementById('menuContent');
+    if (menuContent.style.display === 'block') {
+        menuContent.style.display = 'none';
+    } else {
+        menuContent.style.display = 'block';
+    }
+}
